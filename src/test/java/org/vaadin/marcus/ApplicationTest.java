67c70319e823d60dc41f4a82334b4f94e454a263
SourC <souriya.khaosanga@gmail.com>
2024-09-20 02:10:14 -0700

    fix

--- src/test/java/org/vaadin/marcus/ApplicationTest.java
+++ src/test/java/org/vaadin/marcus/ApplicationTest.java
@@ -5,12 +5,11 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest // No need to exclude anything here
+@SpringBootTest
 public class ApplicationTest {
 
     @Autowired
-    private Application application;
+    private Application application; // Ensure the application is properly injected
 
     @Test
     public void contextLoads() {
